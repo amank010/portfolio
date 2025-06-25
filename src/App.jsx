a71c1dbd29d65167751f73a1897017d22b0a665d
amank010 <amanksingh010@gmail.com>
2025-06-26 03:15:26 +0530

    project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import Projects from './sections/Projects'
 
 const App = () => {
+
+  const [isProject, setIsProject] = useState(false);
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const projectOffset = document.getElementById('projects')?.offsetTop || 0;
+
+      setIsProject(scrollY + window.innerHeight > projectOffset +100);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return()=>window.removeEventListener('scroll', handleScroll);
+  },[]);
+
+
   return (
-    
-    <div className='relative min-h-screen w-full bg-[radial-gradient(ellipse_at_top,_rgba(0,102,255,0.4)_20%,_transparent_90%)] items-center justify-center'>
-      <div className='max-w-8xl mx-auto '>
+    <div className='relative min-h-screen w-full'>
+      {/* blue gradient */}
+      <div className={`absolute inset-0 z-0 transition-opacity duration-1000 pointer-events-none ${isProject ? 'opacity-0' : 'opacity-100'} bg-[radial-gradient(ellipse_at_top,_rgba(0,102,255,0.4)_20%,_transparent_90%)] `}/>
+      
+
+      {/* violet */}
+      <div className={`absolute inset-0 z-0 transition-opacity duration-1000 pointer-events-none ${isProject ? 'opacity-100' : 'opacity-0'} bg-[radial-gradient(ellipse_at_top,_rgba(138,43,226,0.7)_20%,_white_90%)] `}/>
+        
+
+  
+      <div className='relative z-10 max-w-8xl mx-auto '>
       <Navbar/>
       <Hero/>
-
+      <Projects/>
       </div>
+    
     </div>
   )
 }
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section id="projects" className="my-20 sm:px-20 px-10">
+      <h1 className="text-5xl font-medium mb-7">Projects</h1>
+      <div className="grid xl:grid-cols-2 xl:grid-rows-1 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        {" "}
+        {/*  grid */}
+        <div className="col-span-1 xl:row-span-1">
+          {/*  grid container, maybe one project */}
+          <div className="w-full h-full border border-neutral-200  bg-white/20 backdrop-blur-md rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <h1 className="text-2xl font-medium text-center ">
+              Full Stack E-Commerce Web App
+            </h1>
+            <img
+            onClick={()=>window.open("https://e-commerce-frontend-two-beta.vercel.app")}
+              className="cursor-pointer w-full rounded-md h-fit object-contain"
+              src="/assets/forever.png"
+              alt=""
+            />
+            <div className="">
+              <div className="flex justify-between my-2">
+                <button
+                  onClick={() =>
+                    window.open("https://github.com/amank010/E-Commerce-App")
+                  }
+                  className="cursor-pointer border border-black rounded-full px-4 py-1 hover:bg-black hover:text-white ease-in-out duration-300"
+                >
+                  Github
+                </button>
+                <button
+                  onClick={() => window.open("https://e-commerce-frontend-two-beta.vercel.app")}
+                  className="cursor-pointer border border-black rounded-full px-4 py-1 hover:bg-black hover:text-white ease-in-out duration-300">
+                  Live
+                </button>
+              </div>
+              <p className="text-[#656565] text-base">
+                I have created this MERN Fullstack E-Commerce Web App. Including
+                Authentication, Payment Gateway, Admin Panel, and many more
+                features.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="col-span-1 xl:span-3">
+          <div
+          className="w-full h-full border border-neutral-200  bg-white/20 backdrop-blur-md rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <h1 className="text-2xl font-medium text-center">
+              Full Stack Real-Time Chat App
+            </h1>
+            <img
+            onClick={()=>window.open("https://chat-app-frontend-ruddy-omega.vercel.app/login")}
+              className="cursor-pointer w-full rounded-md h-fit object-contain"
+              src="/assets/chatApp.png"
+              alt=""
+            />
+            <div>
+              <div className="flex justify-between my-2">
+                <button
+                onClick={() =>
+                  window.open("https://github.com/amank010/Chat-App")
+                }
+                className="cursor-pointer border border-black rounded-full px-4 py-1 hover:bg-black hover:text-white ease-in-out duration-300">
+                  Github
+                </button>
+                <button
+                onClick={() => window.open("https://chat-app-frontend-ruddy-omega.vercel.app/login")}
+                className="cursor-pointer border border-black rounded-full px-4 py-1 hover:bg-black hover:text-white ease-in-out duration-300">
+                  Live
+                </button>
+              </div>
+              <p className="text-[#656565] text-base">
+                I have created this MERN Fullstack Real-Time Chat App using
+                Socket.io. and including Authentication, Login, Create Account
+                and many more features.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="col-span-1 xl:span-3">
+          <div className="w-full h-full border border-neutral-200  bg-white/20 backdrop-blur-md rounded-lg sm:p-7 p-4 flex flex-col gap-5">
+            <h1 className="text-2xl font-medium text-center">
+              Dijkstra's Algorithm Visualiser
+            </h1>
+            <img
+            onClick={()=>window.open("https://pathfinder-dijkstra-visualiser.vercel.app")}
+              className="cursor-pointer w-full rounded-md h-fit object-contain"
+              src="/assets/dsa.png"
+              alt=""
+            />
+            <div>
+              <div className="flex justify-between my-2">
+                <button
+                onClick={()=>window.open("https://github.com/amank010/pathfinder")}
+                className="cursor-pointer border border-black rounded-full px-4 py-1 hover:bg-black hover:text-white ease-in-out duration-300">
+                  Github
+                </button>
+                <button
+                onClick={() => window.open("https://pathfinder-dijkstra-visualiser.vercel.app")}
+                className="cursor-pointer border border-black rounded-full px-4 py-1 hover:bg-black hover:text-white ease-in-out duration-300">
+                  Live
+                </button>
+              </div>
+              <p className="text-[#656565] text-base">
+                Built an interactive pathfinding visualizer using React.js to
+                animate Dijkstra’s Algorithm on a 2D grid. Implemented
+                grid-based node system with mouse-based wall placement and
+                start/finish node selection{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
